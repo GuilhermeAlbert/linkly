7a6ccdb3893740571e11aaa083274f7945976327
Guilherme Albert <guilhermealbert14@gmail.com>
2024-06-11 08:16:53 -0300

    mount url right

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,4 +1,4 @@
-export function TextInput({ label, ...props }) {
+export function TextInput({ label, error, ...props }) {
   return (
     <div>
       <label
@@ -12,6 +12,12 @@ export function TextInput({ label, ...props }) {
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         {...props}
       />
+
+      {error && (
+        <span className="block mb-2 text-sm font-medium text-red-600 mt-2">
+          {error}
+        </span>
+      )}
     </div>
   );
 }
--- src/components/link/index.tsx
+++ src/components/link/index.tsx
@@ -9,7 +9,9 @@ export function Link({ title, onNewClick }): JSX.Element {
       </span>
 
       <div className="p-6 bg-gray-50 border border-gray-300   dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 rounded text-medium text-lg break-words underline">
-        <a href={title}>{title}</a>
+        <a href={title} target="_blank">
+          {title}
+        </a>
       </div>
 
       <button
